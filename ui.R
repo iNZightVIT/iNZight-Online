0cbf574243fdc2ea9f00ebc9aa98942f382dbc26
sharko666 <christoph.knapp01@gmail.com>
2015-07-22 11:20:18 +1200

    wrapped UI in fluidPage

--- ui.R
+++ ui.R
@@ -1,14 +1,13 @@
 library(shiny)
 
-shinyUI(
-  navbarPage(tags$head(tags$script("alert('Please Note, iNZight Online will soon be replaced by our new improved version iNZight Lite. Go to http://docker.stat.auckland.ac.nz/ to check out our beta version.')")),
-             HTML("<a href='/futurelearn2014/'>iNZight online</a>"),
-             id = "nav_radiant", collapsable = TRUE,
-             tabPanel("Data", uiOutput('data_ui_and_tabs')),
-             tabPanel("Basic", uiOutput("basicdefine")),           
-             tabPanel("TimeSeries", uiOutput('timeseries_ui')),
-             tabPanel("Quick Explore", uiOutput('view_ui_and_tabs')),
-             tabPanel("Model", uiOutput('ui_model')),
-             tabPanel("Home", uiOutput('home_side_and_main'))
-  )
+shinyUI(fluidPage(tags$head(tags$script("alert('Please Note, iNZight Online will soon be replaced by our new improved version iNZight Lite. Go to http://docker.stat.auckland.ac.nz/ to check out our beta version.')")),
+                  navbarPage(HTML("<a href='/futurelearn2014/'>iNZight online</a>"),
+                             id = "nav_radiant", collapsable = TRUE,
+                             tabPanel("Data", uiOutput('data_ui_and_tabs')),
+                             tabPanel("Basic", uiOutput("basicdefine")),
+                             tabPanel("TimeSeries", uiOutput('timeseries_ui')),
+                             tabPanel("Quick Explore", uiOutput('view_ui_and_tabs')),
+                             tabPanel("Model", uiOutput('ui_model')),
+                             tabPanel("Home", uiOutput('home_side_and_main')))
+                  )
 )
